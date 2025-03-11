b5bad5413d40e54cdabc60e21ec111caa882ab6e
archanaanil2025 <archanasiddu2@gmail.com>
2025-03-11 23:10:21 +0530

    Table_9.java

---
+++ src/Table_9.java
@@ -0,0 +1,17 @@
+public class Table_9 {
+    public static void main(String[] args) {
+        int a=9;
+        int b=1;
+        System.out.printf("%d * %d = %d\n",a,b,a*b);
+        System.out.printf("%d * %d = %d\n",a,b+1,a*(b+1));
+        System.out.printf("%d * %d = %d\n",a,b+2,a*(b+2));
+        System.out.printf("%d * %d = %d\n",a,b+3,a*(b+3));
+        System.out.printf("%d * %d = %d\n",a,b+4,a*(b+4));
+        System.out.printf("%d * %d = %d\n",a,b+5,a*(b+5));
+        System.out.printf("%d * %d = %d\n",a,b+6,a*(b+6));
+        System.out.printf("%d * %d = %d\n",a,b+7,a*(b+7));
+        System.out.printf("%d * %d = %d\n",a,b+8,a*(b+8));
+        System.out.printf("%d * %d = %d\n",a,b+9,a*(b+9));
+
+    }
+}
